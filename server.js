400ae0d2325da9435f921138c918ebbbf9950d1a
James Tease <jamestease@gmail.com>
2014-06-24 17:21:10 +0100

    testing aws

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var crypto = require('crypto');
 
 var port = process.env.PORT || 3000;
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
+
+var AWS_ACCESS_KEY = process.env.aws_access_key_id;
+var AWS_SECRET_KEY = process.env.aws_secret_access_key;
+var AWS_BUCKET = process.env.aws_bucket;
 
 app.get('/scrape', function(req, res) {
     var url = 'http://www.bbc.co.uk/weather/2654675';
@@ -31,6 +36,23 @@ app.get('/scrape', function(req, res) {
                 } else {
                     console.log('Wrote json file');
                     res.send('Pollen count updated');
+
+                    // upload to aws
+                    var expires =  Math.ceil((new Date().getTime() + 10000)/1000); // 10 seconds from now
+                    var amz_headers = "x-amz-acl:public-read";
+
+                    var putRequest = "PUT\n\napplication/json\n" + expires + "\n" + amz_headers + "\n" + AWS_BUCKET + "/pollen.json";
+
+                    var signature = crypto.createHmac('sha1', AWS_SECRET_KEY).update(put_request).digest('base64');
+                    signature = encodeURIComponent(signature.trim());
+                    signature = signature.replace('%2B','+');
+                    var url = 'https://'+AWS_BUCKET+'.s3.amazonaws.com/pollen.json';
+                    var credentials = {
+                        signed_request: url+"?AWSAccessKeyId="+AWS_ACCESS_KEY+"&Expires="+expires+"&Signature="+signature,
+                        url: url
+                    };
+                    res.write(JSON.stringify(credentials));
+                    res.end();
                 }
 
             });
