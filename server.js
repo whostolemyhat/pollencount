09db4f3a9f2ad10e69905a5a6f66ec8d6fb045fd
James Tease <jamestease@gmail.com>
2014-06-25 08:03:20 +0100

    updating count

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var crypto = require('crypto');
+// var crypto = require('crypto');
+var s3 = require('aws2js');
 
 var port = process.env.PORT || 3000;
 
@@ -38,26 +39,28 @@ app.get('/scrape', function(req, res) {
                     // res.send('Pollen count updated');
 
                     // upload to aws
-                    var expires =  Math.ceil((new Date().getTime() + 10000)/1000); // 10 seconds from now
-                    var amz_headers = 'x-amz-acl:public-read';
+                    // s3.load('s3', AWS_ACCESS_KEY, AWS_SECRET_KEY);
+                    // s3.setBucket(AWS_BUCKET);
+                    // var expires =  Math.ceil((new Date().getTime() + 10000)/1000); // 10 seconds from now
+                    // var amz_headers = 'x-amz-acl:public-read';
 
-                    var putRequest = 'PUT\n\napplication/json\n' + expires + '\n' + amz_headers + '\n' + AWS_BUCKET + '/pollen.json';
+                    // var putRequest = 'PUT\n\napplication/json\n' + expires + '\n' + amz_headers + '\n' + AWS_BUCKET + '/pollen.json';
 
-                    var signature = crypto.createHmac('sha1', AWS_SECRET_KEY).update(putRequest).digest('base64');
-                    signature = encodeURIComponent(signature.trim());
-                    signature = signature.replace('%2B','+');
-                    var url = 'https://'+AWS_BUCKET+'.s3.amazonaws.com/pollen.json';
-                    var credentials = {
-                        signed_request: url+'?AWSAccessKeyId='+AWS_ACCESS_KEY+'&Expires='+expires+'&Signature='+signature,
-                        url: url
-                    };
-                    res.write(JSON.stringify(credentials));
-                    res.end();
+                    // var signature = crypto.createHmac('sha1', AWS_SECRET_KEY).update(putRequest).digest('base64');
+                    // signature = encodeURIComponent(signature.trim());
+                    // signature = signature.replace('%2B','+');
+                    // var url = 'https://'+AWS_BUCKET+'.s3.amazonaws.com/pollen.json';
+                    // var credentials = {
+                    //     signed_request: url+'?AWSAccessKeyId='+AWS_ACCESS_KEY+'&Expires='+expires+'&Signature='+signature,
+                    //     url: url
+                    // };
+                    // res.write(JSON.stringify(credentials));
+                    // res.end();
                 }
 
             });
 
-            // res.send('finished');
+            res.send('finished');
         }
     });
 });
